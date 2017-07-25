868ee6f2f34dac10a70040486089c7cc84d39a79
Senasoft <owrivero@misena.edu.co>
2017-07-25 16:31:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/senasoft/prueba1/vista/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.senasoft.prueba1.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.senasoft.prueba1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
